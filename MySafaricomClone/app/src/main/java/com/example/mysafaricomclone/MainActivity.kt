d1fcbd9b5dba9f1c2d0ef227a930ba44ee14e197
MUTINDAH <petahmutinda@gmail.com>
2023-05-12 17:14:12 +0300

    MySafaricom Clone App project files

--- MySafaricomClone/app/src/main/java/com/example/mysafaricomclone/MainActivity.kt
+++ MySafaricomClone/app/src/main/java/com/example/mysafaricomclone/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.mysafaricomclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.mysafaricomclone.ui.theme.MySafaricomCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MySafaricomCloneTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background){
+                    Home()
+                    
                 }
             }
         }
@@ -30,15 +32,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MySafaricomCloneTheme {
-        Greeting("Android")
+fun Home(){
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        contentPadding = PaddingValues(8.dp),
+    ) {
+        items(count = 20){numb->
+            MyCard(mytext= "$numb")
+        }
     }
 }
-y
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun MyCard(mytext:String){
+
+    Card(
+        modifier = Modifier
+        .size(100.dp)
+        .padding(3.dp)
+    ){
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = mytext)
+        }
+
+    }
+}
