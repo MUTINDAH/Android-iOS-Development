b0a302d455c18a7c74aa416ffe83a40f190ac837
MUTINDAH <petahmutinda@gmail.com>
2023-05-13 18:06:49 +0300

    MySafaricom Clone App project files

--- MySafaricomClone/app/src/main/java/com/example/mysafaricomclone/MainActivity.kt
+++ MySafaricomClone/app/src/main/java/com/example/mysafaricomclone/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.mysafaricomclone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -11,7 +10,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.mysafaricomclone.ui.theme.MySafaricomCloneTheme
 
@@ -34,14 +32,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Home(){
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
+        columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(8.dp),
+
     ) {
         items(count = 20){numb->
             MyCard(mytext= "$numb")
         }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
@@ -49,8 +49,9 @@ fun MyCard(mytext:String){
 
     Card(
         modifier = Modifier
-        .size(100.dp)
-        .padding(3.dp)
+            .size(100.dp)
+            .padding(3.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
     ){
 
         Column(
